306c2a9d7a84a454bb49ec6f6b102cfc5a69492d
ntcherni <ntcherni@sfu.ca>
2020-03-29 04:49:02 -0700

    simulating various agent actions with plotting now

--- main.js
+++ main.js
@@ -37,6 +37,10 @@ class Agent {
 		this.home = 1; 
 	}
 
+	changeHabits(new_watch) {
+		this.watch = new_watch;
+	}
+
 	arriveHome() {
 		this.home = 1; 
 	}
@@ -50,7 +54,7 @@ class Agent {
 	}
 }
 
-let probs = {
+let probabilities = {
 	'toaster' :  {
 		'day' : {
 			'morning' : 0.2,
@@ -71,15 +75,11 @@ let probs = {
 
 }
 
-let daily = { 
-	'toaster' : 2,
-}
-
 function setProbByHour(appl, t) {
-	if(t >= 6 && t <= 11) appl.setProb(probs[appl.type]['day']['morning']);
-	if(t >= 12 && t <= 18) appl.setProb(probs[appl.type]['day']['afternoon']);	
-	if(t >= 18 && t <= 23) appl.setProb(probs[appl.type]['day']['evening']);	
-	if(t >= 0 && t <= 6) appl.setProb(probs[appl.type]['day']['night']);	
+	if(t >= 6 && t <= 11) appl.setProb(probabilities[appl.type]['day']['morning']);
+	if(t >= 12 && t <= 18) appl.setProb(probabilities[appl.type]['day']['afternoon']);	
+	if(t >= 18 && t <= 23) appl.setProb(probabilities[appl.type]['day']['evening']);	
+	if(t >= 0 && t <= 6) appl.setProb(probabilities[appl.type]['day']['night']);	
 }
 
 function onTimeToSteps(ontime, step_duration) {
@@ -92,12 +92,14 @@ function storeData(x_value, y_value) {
 	data[0].y.push(y_value);
 }
 
-a = new Appliance('toaster', 10, 5, 5);
-a = new Appliance('heater', 40, 10, 30);
+a = new Appliance('heater', 100, 10, 30);
 
-agent = new Agent(5);
+agent = new Agent(0.5);
+agent.changeHabits(0.5);
 
-// --- setup for daily loop:
+// ---- setup for daily loop:
+
+// -- variables for all days:
 
 // empty data object array: 
 const data = [
@@ -108,69 +110,138 @@ const data = [
 	}
 ];
 
+// appliance daily constants: 
+a.max_times = 4; 
 
-// initializing some variables: 
-last_step_on = null;
-times_turned_on = 0;
-max_times_reached = false;
+// for step calculations: 
+step_duration = 5; 
+steps_per_hour = (60 / step_duration);
+steps_per_day = 24 * steps_per_hour;
 
+// for average calculation: 
 total_watts = 0;
 average = 0;
 
-// for step calculations: 
-step_duration = 15; 
-steps_per_hour = (60 / step_duration);
+// -- initializing some variables for 1 day: 
+last_step_on = null;
+times_turned_on_day = 0;
+max_times_reached_day = false;
 
 // set max times for the appliance for the day
-a.max_times = 2; 
 max_times_to_turn_on = 1 + Math.floor(Math.random() * Math.floor(a.max_times));
+
 on_steps = onTimeToSteps(a.ontime, step_duration);
+start_step = 0;
 
-// remaking the above, seems messy
-for(hour = 0; hour < 12; hour++) {
+total_hours = 0; 
 
-	setProbByHour(a, hour);
+days = 200;
 
-	console.log("[" + hour + "]", a.prob);
+// go through as many days as needed 
+for (day = 0; day < days; day++) { 
 
-	start_step = hour * steps_per_hour + 1;
+	// Agent undergoes a change in habits halfway through
+	// the higher the value input into changeHabits function,
+	// the higher the likelihood that the agent will turn off the appliance
+	// instead of simply putting it into standby mode 
+	if(day == 0 ) { agent.changeHabits(0.1); }
+	if(day == 150) { agent.changeHabits(0.9); }
+
+	// simulating a "vacation"
+	if(day == 100) { agent.leaveHome(); }
+	if(day == 150) { agent.arriveHome(); }
 	
-	for(step = start_step; step < (start_step + steps_per_hour); step++) {
+	console.log("day [" + day+ "]");
+
+	// go through one day 
+	for(hour = 0; hour < 24; hour++) {
+
+		// weight the on states by what hour it is 
+		// *this only matters if zooming in to the day
+		setProbByHour(a, hour);
+
+		//console.log("hour [" + hour + "]");
+
+		start_step += steps_per_hour;
+
+		//console.log("start step", start_step)
 		
-		// go through each step in the hour
+		for(step = start_step; step < (start_step + steps_per_hour); step++) {
+			// go through each step in the hour
 
-		if(a.state == -1 || a.state == 0) {
-			// appliance is off or in standby
+			if(a.state == -1 || a.state == 0) {
+				// appliance is off or in standby
 
-			if( !max_times_reached ) {
-				random_chance = Math.random().toFixed(2);
+				if( agent.home == 1 && !max_times_reached_day ) {
+					random_chance = Math.random().toFixed(2);
 
-				if( random_chance > (1.0 - a.prob) ) {
-					// turn appliance on 
-					a.turnOn();
-					last_step_on = step;
-					times_turned_on++; 
-					if (times_turned_on >= max_times_to_turn_on ) max_times_reached = true;
+					if( random_chance > (1.0 - a.prob) ) {
+						// turn appliance on 
+						a.turnOn();
+						last_step_on = step;
+						times_turned_on_day++; 
+						if (times_turned_on_day >= max_times_to_turn_on ) max_times_reached_day = true;
+					}
 				}
+
+			} else if (a.state == 1) {
+				// appliance is on
+				if(step - last_step_on >= on_steps) {
+					// turn appliance off or into standby
+
+					if( Math.random() > (1.0 - agent.watch) ) {
+						console.log("turning off");
+						a.turnOff();
+					} else {
+						console.log("turning standby");
+						a.turnStandby();
+					}
+
+				} 
 			}
+			//	console.log(step, a.state);
 
-		} else if (a.state == 1) {
-			// appliance is on
-			if(step - last_step_on >= on_steps) {
-				// turn appliance off
-				// decide whether to put into ON or STANDBY state
-				// TODO: what is 'decide'?
-
-				a.turnOff();
-			} 
+			// reached end of hour
+			total_watts += a.watts;
+			total_hours++; 
 		}
-		console.log(step, a.state);
-
-		total_watts += a.watts;
-		average = total_watts / step;
-		storeData(step, average);
 	}
+
+	// reached end of day
+
+	console.log("day", day, "this day's total watts: ", total_watts);
+	console.log("total steps:", steps_per_day);	
+	console.log("\n");
+
+	average = total_watts / steps_per_day;
+
+	storeData(day, total_watts);
+
+	// re-initializing some variables for 1 day: 
+	last_step_on = null;
+	times_turned_on_day = 0;
+	max_times_reached_day = false;
+
+	total_watts = 0;
+
+	// set max times for the appliance for the day
+	max_times_to_turn_on = 1 + Math.floor(Math.random() * Math.floor(a.max_times));
 }
+
+// finish up, display plot
+//console.log(data);
+
+var layout = {
+	title: 'Appliance Energy Usage - ' + a.type,
+	xaxis: {
+	  title: 'Day',
+	},
+	yaxis: {
+	  title: 'Watts Used Per Day',
+	}
+  };
+
+plotlib.plot(data, layout);
 
 	// turn appliance ON
 	// * happens when chance is sufficient
@@ -190,9 +261,7 @@ for(hour = 0; hour < 12; hour++) {
 	// * happens on the step when last step on subtracted from the step count 
 	//
 
-console.log(data);
-	
-plotlib.plot(data);
+
 
 // TODO: create X and Y values for Plots 
 // X values = Steps
